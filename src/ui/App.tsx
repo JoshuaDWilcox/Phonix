9e51089bdc0e881d58c56de161a9c0dad112b4c1
dheerajmb <dheerajmb19@gmail.com>
2025-11-03 22:55:55 -0600

    Refined ui with logo

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,28 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import PhonixSquareLogo from './assets/phonixSquareLogo.jpg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isRecording, setIsRecording] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <img src={PhonixSquareLogo} className="logo react" alt="React logo" />
       </div>
-      <h1>Vite + React</h1>
+      <h1></h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setIsRecording((isRecording) => (isRecording === 0) ? 1 : 0)}>
+            {isRecording === 1 ? "Stop Recording" : "Start Recording"}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Profiles button coming soon
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+
       </p>
     </>
   )
